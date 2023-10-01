deac5e18fbd63abedfc2511b00ce46ce4ec12fe3
baiyao <18303620306@163.com>
2023-10-01 12:43:48 +0800

    2023-10-01

---
+++ algorithm/sorting/10种经典排序算法.groovy
@@ -0,0 +1,237 @@
+﻿// 0. 校验排序算法正确性的小工具
+/**
+ * @Author: baiYao
+ * @Date: 2023/10/01/10:32
+ * @Description: 使用Arrays.sort(arr)校验排序算法的正确性
+
+    思路很简单: new一个指定长度的数组, 随机赋值. 使用Arrays.sort(arr)排序
+               复制随机数组, 使用咱们自己写的算法排下序, 最后比较每个位置元素是否相同, 一旦有一个位置不一样就说明写的不对
+
+               循环上面过程3000次, 排除数组的偶然性
+ */
+public class SortChecker {
+    public static void main(String[] args) {
+        check(3000);
+    }
+    static int[] randomArray(int length) {
+        Random random = new Random();
+
+        int[] nums = new int[length];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(length);
+        }
+
+        return nums;
+    }
+
+    static void check(int loopTimes) {
+        boolean isEqual = true;
+
+        first: for (int times = 0; times < loopTimes; times++) {
+            int[] numsA = randomArray(loopTimes);
+            int[] numsB = new int[numsA.length];
+
+            System.arraycopy(numsA, 0, numsB, 0, numsA.length);
+
+            Arrays.sort(numsA);
+            QuickSort.quickSort(0, numsB.length - 1, numsB);
+
+            for (int i = 0; i < numsB.length; i++) {
+                if (numsA[i] != numsB[i]) {
+                    isEqual = false;
+
+                    print(numsA);
+
+                    break first;
+                }
+
+            }
+        }
+        if (isEqual) {
+            System.out.println("正确");
+        }
+    }
+
+    static void print(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+
+// 1. 冒泡排序
+1.1 思路
+假设n个元素, 那么就会有n-1轮冒泡
+
+第一轮, 从头开始, 元素从前到后两两比较, 将最大的元素冒泡至最后一个位置
+第二轮, 从头开始, 元素从前到后两两比较, 将最大的元素冒泡至倒数第二个位置
+...
+第n-1轮, 从头开始, 元素从前到后两两比较, 将最大的元素冒泡至倒数第n-1个位置, 也就是正数第二个位置
+
+正序排序完成.
+
+1.2 双循环
+public static void bubbleSort(int[] intArr) {
+    // 1. 一共需要比较 (元素总数-1) 趟
+    for (int i = 0; i < intArr.length - 1; i++) {
+        // 2. 对于每一趟, 要保证后一个元素 (j + 1) < lentgh, 同时考虑每一趟走完, 就会在末尾数排好一个位置, 
+        //    那么下一趟, 排好的这个位置及以后的元素就不需要被比较了, 所以实际上得保证 (j + 1) < lentgh - i 也就是 j < intArr.length - i - 1
+        for (int j = 0; j < intArr.length - i - 1; j++) {
+            if (intArr[j + 1] < intArr[j]) {
+                int temp = intArr[j + 1];
+                intArr[j + 1] = intArr[j];
+                intArr[j] = temp;
+            }
+        }
+    }
+}
+
+1.3 递归 -> 递归效率没有双循环高
+// 1. 需要给出两个参数: 数组, 长度
+public static void bubbleSort(int[] intArr, int length) {
+    // 2. 如果传入的长度是一个直接方法返回
+    if (length < 2) return;
+
+    // 3. 每一轮找出这一轮最大的放在最后面的位置
+    //    代码中只需要保证第(i + 1)的元素的位置最大是length - 1
+    for (int i = 0; i < length - 1; i++) {
+        if (intArr[i + 1] < intArr[i]) {
+            int temp = intArr[i + 1];
+            intArr[i + 1] = intArr[i];
+            intArr[i] = temp;
+        }
+    }
+
+    // 4. 每一轮长度减一
+    bubbleSort(intArr, --length);
+}
+
+1.4 优化 -> 1,2,3,4,5 本来就有序, 你排了一轮之后, 未交换任何两个元素之间的位置, 那么就提前返回
+
+// 只要某一轮过后没有交换任何两位元素的位置, 说明前面的元素都已经正序了, 直接返回即可, 后续排序不需要再进行了
+
+// 1.4.1 双循环优化版
+public static void bubbleSort(int[] intArr) {
+    for (int i = 0; i < intArr.length - 1; i++) {
+
+        boolean ifSwap = false;
+
+        for (int j = 0; j < intArr.length - i - 1; j++) {
+            if (intArr[j + 1] < intArr[j]) {
+                int temp = intArr[j + 1];
+                intArr[j + 1] = intArr[j];
+                intArr[j] = temp;
+
+                ifSwap = true;
+            }
+        }
+
+        if (!ifSwap) return;
+    }
+}
+
+// 1.4.2 递归优化版
+public static void bubbleSort(int[] intArr, int length) {
+    if (length < 2) return;
+
+    boolean ifSwap = false;
+
+    for (int i = 0; i < length - 1; i++) {
+        if (intArr[i + 1] < intArr[i]) {
+            int temp = intArr[i + 1];
+            intArr[i + 1] = intArr[i];
+            intArr[i] = temp;
+
+            ifSwap = true;
+        }
+    }
+
+    if (!ifSwap) return;
+
+    bubbleSort(intArr, --length);
+}
+
+// 5. 快速排序
+5.1 思路
+
+    ① i指针: 范围[start, end - 1], 是为了从左到右扫描出比中心轴位置的数小的值
+    ② pivot指针: 指向中心轴位置
+    ③ pivotIndex指针: 指向扩张位置; 由于所有小的都放左边, 所以pivotIndex用来指向当前被扫描到的小的数应该放的位置
+
+    *
+    *      i                    pivot
+    *      ↓                    ↓
+    *      5, 7, 1, 3, 2, 8, 6, 4
+    *      ↑
+    *      pivotIndex
+    *
+
+    *      -> i从左到右扫描, 发现1比4小, 所有把1放在pivotIndex的位置（也就是第一个位置, 交换位置元素）, pivotIndex索引指向下一个位置
+    *            i              pivot
+    *            ↓              ↓
+    *      1, 7, 5, 3, 2, 8, 6, 4
+    *         ↑
+    *         pivotIndex
+    *
+
+    *      -> i继续往下扫描, 发现3比4小, 所有把3放在第二个位置, pivotIndex索引指向第三个位置
+    *               i           pivot
+    *               ↓           ↓
+    *      1, 3, 5, 7, 2, 8, 6, 4
+    *            ↑
+    *            pivotIndex
+    *
+
+    *      -> i继续往下扫描, 发现2比4小, 所有把2放在第三个位置, pivotIndex索引指向第四个位置
+    *                  i        pivot
+    *                  ↓        ↓
+    *      1, 3, 2, 7, 5, 8, 6, 4
+    *               ↑
+    *               pivotIndex
+    *
+
+    *      -> i继续往下扫描, 发现没有比4小的元素, 最后将pivot位置元素放在pivotIndex位置（交换元素位置）
+    *                           pivot
+    *                           ↓
+    *      1, 3, 2, 4, 5, 8, 6, 7
+    *               ↑
+    *               pivotIndex
+    *
+
+    *      可以发现上面元素中pivotIndex位置确定后, 左侧(1, 3, 2)都比4小, 右侧(5, 8, 6, 7)都比4大
+
+
+5.2 代码
+public class QuickSort {
+    public static void quickSort(int start, int end, int[] nums) {
+        // 保证数组长度不为1的情况下, 找出中心轴位置, 继续分区排序
+        if (start < end) {
+            int pivot = partition(start, end, nums);
+            quickSort(start, pivot - 1, nums);
+            quickSort(pivot + 1, end, nums);
+        }
+    }
+
+    public static int partition(int start, int end, int[] nums) {
+        // 中心轴是最右的位置; pivotIndex是扩张位置, 当然最终指向的是中心轴位置
+        int pivot = end;
+        int pivotIndex = start;
+
+        for (int i = start; i < end; i++) {
+            if (nums[i] < nums[pivot]) {
+                swap(nums, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+        swap(nums, pivotIndex, pivot);
+        return pivotIndex;
+    }
+
+    public static void swap(int[] intArr, int indexA, int indexB) {
+        int temp = intArr[indexB];
+        intArr[indexB] = intArr[indexA];
+        intArr[indexA] = temp;
+    }
+}
---
+++ flink/flinkApi Join&flinkSQL Join.groovy
@@ -0,0 +1,349 @@
+﻿// 1. flink多流合并
+1.1 union -> 可以连接n条流, 但n条流的数据类型必须得一致
+
+DataStreamSource<Integer> ds1 = env.fromElements(1, 2, 3);
+DataStreamSource<Integer> ds2 = env.fromElements(2, 2, 3);
+DataStreamSource<String> ds3 = env.fromElements("2", "2", "3");
+
+ds1
+    .union(
+        ds2,
+        ds3.map(Integer::valueOf)
+    )
+    .print();
+
+1.2 connect -> 一次只能连接 2条流; 流的数据类型可以不一样; 连接后可以调用 map、flatmap、process来处理, 但是各处理各的
+
+1.2.1 .map() -> 传入一个CoMapFunction, 需要实现map1()、map2()两个方法
+
+DataStreamSource<Integer> source1 = env.fromElements(1, 2, 3);
+DataStreamSource<String> source2 = env.fromElements("a", "b", "c");
+
+ConnectedStreams<Integer, String> connect = source1.connect(source2);
+
+connect
+    .map(
+        new CoMapFunction<Integer, String, String>() {
+            @Override
+            public String map1(Integer value) throws Exception {
+                return "来源于数字流:" + value.toString();
+            }
+
+            @Override
+            public String map2(String value) throws Exception {
+                return "来源于字母流:" + value;
+            }   
+        }
+    );
+
+1.2.2 .process() -> 传入一个CoProcessFunction, 需要实现processElement1()、processElement2()两个方法
+// 实现inner join的效果
+source1 = env.fromElements(
+    Tuple2.of(1, "a1"),
+    Tuple2.of(1, "a2"),
+    Tuple2.of(2, "b"),
+    Tuple2.of(3, "c")
+);
+
+source2 = env.fromElements(
+    Tuple3.of(1, "aa1", 1),
+    Tuple3.of(1, "aa2", 2),
+    Tuple3.of(2, "bb", 1),
+    Tuple3.of(3, "cc", 1)
+);
+
+ConnectedStreams<Tuple2<Integer, String>, Tuple3<Integer, String, Integer>> connect = source1.connect(source2);
+
+// 多并行度下, 需要根据 关联条件 进行keyby, 才能保证key相同的数据到一起去, 才能匹配上
+connect
+    .keyBy(s1 -> s1.f0, s2 -> s2.f0)
+    .process(
+        new CoProcessFunction<Tuple2<Integer, String>, Tuple3<Integer, String, Integer>, String>() {
+
+            // 定义 HashMap, 缓存来过的数据, key=id, value=list<数据>
+            Map<Integer, List<Tuple2<Integer, String>>> s1Cache = new HashMap<>();
+            Map<Integer, List<Tuple3<Integer, String, Integer>>> s2Cache = new HashMap<>();
+
+            @Override
+            public void processElement1(Tuple2<Integer, String> value, Context ctx, Collector<String> out) throws Exception {
+                Integer id = value.f0;
+                // TODO 1.来过的s1数据, 都存起来
+                if (!s1Cache.containsKey(id)) {
+                    // 1.1 第一条数据, 初始化 value的list, 放入 hashmap
+                    List<Tuple2<Integer, String>> s1Values = new ArrayList<>();
+                    s1Values.add(value);
+                    s1Cache.put(id, s1Values);
+                } else {
+                    // 1.2 不是第一条, 直接添加到 list中
+                    s1Cache.get(id).add(value);
+                }
+
+                //TODO 2.根据id, 查找s2的数据, 只输出 匹配上 的数据
+                if (s2Cache.containsKey(id)) {
+                    for (Tuple3<Integer, String, Integer> s2Element : s2Cache.get(id)) {
+                        out.collect("s1:" + value + "<--------->s2:" + s2Element);
+                    }
+                }
+            }
+
+            @Override
+            public void processElement2(Tuple3<Integer, String, Integer> value, Context ctx, Collector<String> out) throws Exception {
+                Integer id = value.f0;
+                // TODO 1.来过的s2数据, 都存起来
+                if (!s2Cache.containsKey(id)) {
+                    // 1.1 第一条数据, 初始化 value的list, 放入 hashmap
+                    List<Tuple3<Integer, String, Integer>> s2Values = new ArrayList<>();
+                    s2Values.add(value);
+                    s2Cache.put(id, s2Values);
+                } else {
+                    // 1.2 不是第一条, 直接添加到 list中
+                    s2Cache.get(id).add(value);
+                }
+
+                //TODO 2.根据id, 查找s1的数据, 只输出 匹配上 的数据
+                if (s1Cache.containsKey(id)) {
+                    for (Tuple2<Integer, String> s1Element : s1Cache.get(id)) {
+                        out.collect("s1:" + s1Element + "<--------->s2:" + value);
+                    }
+                }
+            }
+        });
+
+result.print(); 
+
+1.3 window Join
+
+   /**
+    * 类似于inner join
+    * 最后输出的只有两条流中按key配对成功的数据
+    * 如果某个窗口中一条流的数据没有任何另一条流的数据匹配, 那么就不会调用JoinFunction的join()方法, 也就没有任何输出了
+
+    * 效果类似于: SELECT * FROM table1 t1, table2 t2 WHERE t1.id = t2.id; 
+
+    */
+
+stream1
+    .join(stream2)
+    // 指定第一条流的key
+    .where(<KeySelector>)
+    // 指定第二条流的key
+    .equalTo(<KeySelector>)
+    // 滚动, 滑动, 会话 均可
+    .window(<WindowAssigner>)
+    // 这里只能调.apply(), 传入JoinFunction接口, 实现.join()方法
+    .apply(<JoinFunction>)
+
+// ****************************************************************
+
+ds1 = env
+    .fromElements(
+        Tuple2.of("a", 1),
+        Tuple2.of("a", 2),
+        Tuple2.of("b", 3),
+        Tuple2.of("c", 4)
+    )
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple2<String, Integer>>forMonotonousTimestamps()
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+
+ds2 = env
+    .fromElements(
+        Tuple3.of("a", 1,1),
+        Tuple3.of("a", 11,1),
+        Tuple3.of("b", 2,1),
+        Tuple3.of("b", 12,1),
+        Tuple3.of("c", 14,1),
+        Tuple3.of("d", 15,1)
+    )
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple3<String, Integer,Integer>>forMonotonousTimestamps()
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+// TODO window join
+// 1. 落在同一个时间窗口范围内才能匹配
+// 2. 根据keyby的key, 来进行匹配关联
+// 3. 只能拿到匹配上的数据, 类似有固定时间范围的inner join
+
+ds1
+    .join(ds2)
+    .where(r1 -> r1.f0)  // ds1的keyby
+    .equalTo(r2 -> r2.f0) // ds2的keyby
+    .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+    .apply(
+        new JoinFunction<Tuple2<String, Integer>, Tuple3<String, Integer, Integer>, String>() {
+            /**
+                * 关联上的数据, 调用join方法
+                * @param first  ds1的数据
+                * @param second ds2的数据
+                */
+            @Override
+            public String join(Tuple2<String, Integer> first, Tuple3<String, Integer, Integer> second) throws Exception {
+                return first + "<----->" + second;
+            }
+        }
+    );
+
+1.4 interval Join -> 间隔联结的两条流A和B, 必须基于相同的key, 且只支持事件时间语义
+
+   /**
+    * 针对一条流的每个数据, 开辟出其时间戳前后的一段闭区间时间间隔 [a.timestamp + lowerBound, a.timestamp + upperBound]
+    * 看这期间是否有来自另一条流的数据匹配。这段时间作为可以匹配另一条流数据的 "窗口" 范围。
+
+    * interval Join同样是内连接(inner join), 但是跟window Join相比, interval join的"窗口"范围基于流中数据, 所以不确定
+    * 而且流B中的数据可以不只在一个区间内被匹配。
+
+    */
+
+stream1
+    .keyBy(<KeySelector>)
+    .intervalJoin(stream2.keyBy(<KeySelector>))
+    .between(Time.milliseconds(-2), Time.milliseconds(1))
+    .process(
+        new ProcessJoinFunction<Integer, Integer, String(){
+            @Override
+            public void processElement(Integer left, Integer right, Context ctx, Collector<String> out) {
+                out.collect(left + "," + right);
+            }
+        }
+    );
+
+1.4.1 正常使用案例
+
+有两条流, 一条是下单流, 一条是浏览数据的流。针对同一用户, 来做联结。也就是使用一个用户的下单事件和这个用户的最近十分钟的浏览数据进行一个联结查询。
+
+ds1 = env
+    .fromElements(
+        Tuple2.of("a", 1),
+        Tuple2.of("a", 2),
+        Tuple2.of("b", 3),
+        Tuple2.of("c", 4)
+    )
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple2<String, Integer>>forMonotonousTimestamps()
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+
+ds2 = env
+    .fromElements(
+        Tuple3.of("a", 1, 1),
+        Tuple3.of("a", 11, 1),
+        Tuple3.of("b", 2, 1),
+        Tuple3.of("b", 12, 1),
+        Tuple3.of("c", 14, 1),
+        Tuple3.of("d", 15, 1)
+    )
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple3<String, Integer, Integer>>forMonotonousTimestamps()
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+    // TODO interval join
+    //1. 分别做keyby, key其实就是关联条件
+    ds1.keyBy(r1 -> r1.f0);
+    ds2.keyBy(r2 -> r2.f0);
+
+    //2. 调用 interval join
+    ks1.intervalJoin(ks2)
+        .between(Time.seconds(-2), Time.seconds(2))
+        .process(
+            new ProcessJoinFunction<Tuple2<String, Integer>, Tuple3<String, Integer, Integer>, String>() {
+                /**
+                    * 两条流的数据匹配上, 才会调用这个方法
+                    * @param left  ks1的数据
+                    * @param right ks2的数据
+                    * @param ctx   上下文
+                    * @param out   采集器
+                    */
+                @Override
+                public void processElement(Tuple2<String, Integer> left, Tuple3<String, Integer, Integer> right, Context ctx, Collector<String> out) throws Exception {
+                    // 进入这个方法, 是关联上的数据
+                    out.collect(left + "<------>" + right);
+                }
+            }
+        )
+        .print();
+
+1.4.2 处理迟到数据
+
+ds1 = env
+    .socketTextStream("hadoop102", 7777)
+    .map(new MapFunction<String, Tuple2<String, Integer>>() {
+        @Override
+        public Tuple2<String, Integer> map(String value) throws Exception {
+            String[] datas = value.split(",");
+            return Tuple2.of(datas[0], Integer.valueOf(datas[1]));
+        }
+    })
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple2<String, Integer>>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+
+ds2 = env
+    .socketTextStream("hadoop102", 8888)
+    .map(new MapFunction<String, Tuple3<String, Integer, Integer>>() {
+        @Override
+        public Tuple3<String, Integer, Integer> map(String value) throws Exception {
+            String[] datas = value.split(",");
+            return Tuple3.of(datas[0], Integer.valueOf(datas[1]), Integer.valueOf(datas[2]));
+        }
+    })
+    .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+            .<Tuple3<String, Integer, Integer>>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+            .withTimestampAssigner((value, ts) -> value.f1 * 1000L)
+    );
+
+/**
+    * TODO Interval join
+    * 1、只支持事件时间
+    * 2、指定上界、下界的偏移, 负号代表时间往前, 正号代表时间往后
+    * 3、process中, 只能处理 join上的数据
+    * 4、两条流关联后的watermark, 以两条流中最小的为准
+    * 5、如果 当前数据的事件时间 < 当前的watermark, 就是迟到数据,  主流的process不处理
+    *  => between后, 可以指定将 左流 或 右流 的迟到数据 放入侧输出流
+    */
+
+//1. 分别做keyby, key其实就是关联条件
+ks1 = ds1.keyBy(r1 -> r1.f0);
+ks2 = ds2.keyBy(r2 -> r2.f0);
+
+//2. 调用 interval join
+ks1LateTag = new OutputTag<>("ks1-late", Types.TUPLE(Types.STRING, Types.INT));
+ks2LateTag = new OutputTag<>("ks2-late", Types.TUPLE(Types.STRING, Types.INT, Types.INT));
+
+ks1
+    .intervalJoin(ks2)
+    .between(Time.seconds(-2), Time.seconds(2))
+    .sideOutputLeftLateData(ks1LateTag)  // 将 ks1的迟到数据, 放入侧输出流
+    .sideOutputRightLateData(ks2LateTag) // 将 ks2的迟到数据, 放入侧输出流
+    .process(
+        new ProcessJoinFunction<Tuple2<String, Integer>, Tuple3<String, Integer, Integer>, String>() {
+            /**
+                * 两条流的数据匹配上, 才会调用这个方法
+                * @param left  ks1的数据
+                * @param right ks2的数据
+                * @param ctx   上下文
+                * @param out   采集器
+                */
+            @Override
+            public void processElement(Tuple2<String, Integer> left, Tuple3<String, Integer, Integer> right, Context ctx, Collector<String> out) throws Exception {
+                // 进入这个方法, 是关联上的数据
+                out.collect(left + "<------>" + right);
+            }
+        }
+    );
+
+process.print("主流");
+process.getSideOutput(ks1LateTag).printToErr("ks1迟到数据");
+process.getSideOutput(ks2LateTag).printToErr("ks2迟到数据");
